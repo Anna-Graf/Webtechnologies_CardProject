f764624b7dcf13aae6058fe97b68a8a66666367c
Anna Graf <anna.graf@zhdk.ch>
2025-06-09 18:38:47 +0200

    ingredients

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log("...fetching a random cocktail 🍹");
 // prepare dom elements
 const titleContainer = document.querySelector("[data-js='title']");
 const imgContainer = document.querySelector("[data-js='photo']");
+const ingredientsContainer = document.querySelector("[data-js='ingredients']")
+const prepContainer = document.querySelector("[data-js='preparation']")
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .then((response) => response.json())
@@ -25,6 +27,22 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     img.src = drink.strDrinkThumb;
     img.alt = drink.strDrink;
     imgContainer.appendChild(img);
+
+    //ingredients
+    const ingredients = [];
+  for (let i = 1; i <= 15; i++) {
+  const ingredient = drink[`strIngredient${i}`];
+  const measure = drink[`strMeasure${i}`];
+  if (ingredient) {
+    ingredients.push(`${measure ? measure.trim() : ""} ${ingredient.trim()}`);
+  }
+}
+
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  ingredientsContainer.appendChild(li);
+});
   })
   .catch((error) => {
     // Handle any errors that occur during the fetch request
